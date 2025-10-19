bd390ddd22b90012d0122aa667fa8440583946de
SajariS <santeri.sajari@gmail.com>
2025-10-19 15:49:14 +0300

    Flask API tests

--- src/server.py
+++ src/server.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask import request, jsonify
+from .routes.testi import testi_bp
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello world, flask toimii :D"
+app.register_blueprint(testi_bp)
 
 def main():
     app.run(debug=True)
